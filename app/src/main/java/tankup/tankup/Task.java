0503ad58e1926f7c9e1e24468e1db6418347eb02
Paratronix <40120236+Paratronix@users.noreply.github.com>
2018-06-10 13:47:44 +0200

    Revert "Revert "add""
    This reverts commit 7ef8e415c209732daab793c8f133166556fe15c3.

---
+++ app/src/main/java/tankup/tankup/Task.java
@@ -0,0 +1,29 @@
+package tankup.tankup;
+
+public class Task {
+    private String task;
+
+
+    public Task(){
+
+    }
+
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public Task(String task) {
+        this.task = task;
+
+
+
+
+
+
+    }
+}
